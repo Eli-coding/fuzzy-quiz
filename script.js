09d11ef3836b816a2daf64090fd88b3b6e276edf
Elizabeth Rodriguez <elizabethyoannie@outlook.com>
2021-12-19 20:52:54 -0400

    code

---
+++ script.js
@@ -0,0 +1,24 @@
+//in java script 
+
+var questions = [
+    {
+        questionNumber: "1",
+        questionWording: "What is a bear?",
+        choices: ["carrot", "sky", "pizza"],
+        answer: ["sky"]
+    },
+    {
+        questionNumber: "2",
+        questionWording: "What is a plaza?",
+        choices: ["carrot", "sky", "pizza"],
+        answer: ["pizza"]
+    }
+
+] 
+
+for(
+    if (choices === answer){
+
+}
+//start with html
+//start button
