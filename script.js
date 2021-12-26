327a42cbe510968d5cee4051730f199865d2fef0
Elizabeth Rodriguez <elizabethyoannie@outlook.com>
2021-12-26 19:33:39 -0400

    all this is a work in progress

--- script.js
+++ script.js
@@ -1,76 +1,72 @@
-
-//Questions - Answers - Message 
-var questionTracker = 0;
-
-function displayQuestion(qIndex) {
-
-    var h1 = document.createElement('h1');
-    h1.textContent = questions[qIndex].questionWording;
-    document.getElementById('content').append(h1);
-
-    for (var i = 0; i < questions[qIndex].choices.length; i++) {
-        var button = document.createElement('button');
-        button.setAttribute('class', 'optionSelected')
-        button.textContent = questions[qIndex].choices[i];
-
-        document.getElementById('content').append(button);
-
-    }
-
-
-}
-
-//fix event listener for button options
-
-var questions = [
+const questions = [
     {
-        
+
         questionWording: "1. Commonly use data types DO NOT include",
-        choices: ["string", "booleans", "numbers"], //buttons
-        answer: ["booleans"]
+        choices: ["string", "booleans", "numbers"], 
+                         
+        answer: "booleans"
     },
     {
-        
+
         questionWording: "2. Arrays in Javascript can be used to store?",
         choices: ["numbers", "strings", "All of the above"],
-        answer: ["All of the above"]
+
+        answer: "All of the above"
     },
     {
-        
+
         questionWording: "3. Inside which HTML element do we put javascript?",
         choices: ["<js>", "<h2>", "<script>"],
-        answer: ["<script>"]
+
+        answer: "<script>"
     },
     {
-        
+
         questionWording: "4. Where is the correct place to insert a JavaScript?",
         choices: ["<head>", "<body>", "both are correct"],
-        answer: ["both are correct"]
+
+        answer: "both are correct"
     },
     {
-        
+
         questionWording: "5. How do you create a function in JavaScript?",
         choices: ["function myFunction()", "function: myFunction()", "function = myFunction()"],
-        answer: ["function myFunction()"]
+
+        answer: "function myFunction()"
     },
 
-]
+] 
 
+var quizContainer = document.getElementById('quizContainer');
+var resultsContainer = document.getElementById('output');
 
-//checks answers 
-for (i = 0; i >= questions.length; i++) {
-    if (choices === answer) {
-        document.write("Correct!");
+document.getElementById("startButton").addEventListener("click", displayQuestion);
+var questionTracker = 0;
 
-    } else {
-        document.write("Wrong!")
-        //subtracts time to timer
-    }
+// displays questions 
+function displayQuestion() {
+
+    console.log("hi from displayquestions");
+
+    var theQuestion = document.getElementById("quizContainer"); 
+    console.log(theQuestion);
+
+    theQuestion.innerHTML = "Hello World";
 }
-// Highscores takes intitials and score
+
+//checks questions and substracts timer if needed
+function checkAnswers() {
+    
+  
+}
+
 
 //timer 
-var timeLeft = 90
+
+function Timer()
+{
+
+    var timeLeft = 90
 // Selects element by class
 var timeEl = document.querySelector(".time");
 
@@ -98,34 +94,18 @@ function setTime() {
 }
 
 
-function Highscores() {
-    document.write("Enter initials:")
-    //input 
-    // local memory 
 }
 
-document.getElementById("button").addEventListener("click", function () {
+// Highscores takes intitials and score
+function Highscores() {
+    
+    var score = localStorage.getItem ("score"); // get it from storage
+    var initials = localStorage.getItem ("initial"); 
+
+    localStorage.setItem ("score", score ); //store in local storage
+    localStorage.setItem ("initial", initials ); 
+}
 
 
-    displayQuestion(questionTracker);
 
 
-    var options = document.getElementsByClassName('optionSelected');
-    console.log('test', options);
-
-   for (var i = 0; i < options.length; i++) {
-        options[i].addEventListener("click", function () {
-            questionTracker++
-            document.getElementById('content').innerHTML = "";
-            displayQuestion(questionTracker);
-        })
-    }
-
-})
-
-
-//WHEN all questions are answered or the timer reaches 0
-//THEN the game is over
-//WHEN the game is over
-//THEN I can save my initials and my score
-// click start to begin questions and timer
